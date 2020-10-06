36566b85619f7306775573d5f05b538088cf24f7
linqi <857714244@qq.com>
2020-10-06 11:44:30 +0800

    http请求

---
+++ Week_06/server.js
@@ -0,0 +1,19 @@
+const http = require('http')
+
+http.createServer((req, res) => {
+    let body = []
+    req.on('error', (err) => {
+        console.log('=====>')
+        console.error(err)
+    }).on('data', (chunk) => {
+        console.log('chunk', chunk)
+        body.push(chunk)
+    }).on('end', () => {
+        body = Buffer.concat(body).toString()
+        console.log('body:', body)
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end(' Hello World\n')
+    })
+}).listen(8080)
+
+console.log('server started')
