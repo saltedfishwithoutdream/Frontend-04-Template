4e60f4753be4867a77db96080cc584b6177d435f
linqi <857714244@qq.com>
2021-01-02 12:09:47 +0800

    17周

---
+++ Week_17/toytool/src/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>{{gretting}} World</p>
+</tempalte>
+
+<script>
+    module.exports = {
+        data: function () {
+            return {
+                greeting: 'Hello'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    p {
+        font-size: 2rem;
+        text-align: center;
+    }
+</style>
